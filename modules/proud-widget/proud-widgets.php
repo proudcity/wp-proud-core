0ca282e46d7fbc548d5f9433ee58249a35de0633
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-08 00:01:44 -0800

    Adding submenu widget

--- modules/proud-widget/proud-widgets.php
+++ modules/proud-widget/proud-widgets.php
@@ -6,3 +6,4 @@
 require_once plugin_dir_path(__FILE__) . '/widgets/base/widget.class.php';
 require_once plugin_dir_path(__FILE__) . '/widgets/icon-link/icon-link-widget.class.php';
 require_once plugin_dir_path(__FILE__) . '/widgets/teaser-list/teaser-list-widget.class.php';
+require_once plugin_dir_path(__FILE__) . '/widgets/submenu/submenu-widget.class.php';
