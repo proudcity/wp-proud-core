a1a5aba3872789bbe05686565f440cd2bec6120d
Jeff Lyon <jeff@albatrossdigital.com>
2016-01-29 02:48:51 -0800

    Adding agency proud layout

--- modules/so-pagebuilder/proud-so-pagebuilder.php
+++ modules/so-pagebuilder/proud-so-pagebuilder.php
@@ -123,9 +123,140 @@ class ProudSOPagebuilder {
 
 
   function proud_prebuilt_layouts($layouts){
+    
+    $layouts['agency-page'] = array(
+      'name' => __('Agency home page', 'proud'),    
+      'description' => __('Agency header and sidebar with contact info', 'proud'),    // Optional
+      'widgets' => 
+      array (
+        0 => 
+        array (
+          'text' => '<h1>[title]</h1>',
+          'headertype' => 'header',
+          'background' => 'image',
+          'pattern' => '',
+          'repeat' => 'full',
+          'image' => '[featured-image]',
+          'make_inverse' => 'yes',
+          'panels_info' => 
+          array (
+            'class' => 'JumbotronHeader',
+            'grid' => 0,
+            'cell' => 0,
+            'id' => 0,
+          ),
+        ),
+        1 => 
+        array (
+          'title' => '',
+          'panels_info' => 
+          array (
+            'class' => 'AgencyMenu',
+            'raw' => false,
+            'grid' => 1,
+            'cell' => 0,
+            'id' => 1,
+          ),
+        ),
+        2 => 
+        array (
+          'title' => '',
+          'panels_info' => 
+          array (
+            'class' => 'AgencySocial',
+            'raw' => false,
+            'grid' => 1,
+            'cell' => 0,
+            'id' => 2,
+          ),
+        ),
+        3 => 
+        array (
+          'title' => 'Contact',
+          'panels_info' => 
+          array (
+            'class' => 'AgencyContact',
+            'raw' => false,
+            'grid' => 1,
+            'cell' => 0,
+            'id' => 3,
+          ),
+        ),
+        4 => 
+        array (
+          'title' => 'Hours',
+          'panels_info' => 
+          array (
+            'class' => 'AgencyHours',
+            'raw' => false,
+            'grid' => 1,
+            'cell' => 0,
+            'id' => 4,
+          ),
+        ),
+        5 => 
+          array (
+            'title' => '',
+            'text' => '',
+            'text_selected_editor' => 'tinymce',
+            'autop' => true,
+            '_sow_form_id' => '56ab38067a600',
+            'panels_info' => 
+            array (
+              'class' => 'SiteOrigin_Widget_Editor_Widget',
+              'grid' => 1,
+              'cell' => 1,
+              'id' => 5,
+              'style' => 
+              array (
+                'background_image_attachment' => false,
+                'background_display' => 'tile',
+              ),
+            ),
+          ),
+      ),
+      'grids' => 
+      array (
+        0 => 
+        array (
+          'cells' => 1,
+          'style' => 
+          array (
+            'row_stretch' => 'full',
+            'background_display' => 'tile',
+          ),
+        ),
+        1 => 
+        array (
+          'cells' => 2,
+          'style' => 
+          array (
+          ),
+        ),
+      ),
+      'grid_cells' => 
+      array (
+        0 => 
+        array (
+          'grid' => 0,
+          'weight' => 1,
+        ),
+        1 => 
+        array (
+          'grid' => 1,
+          'weight' => 0.33345145287029998,
+        ),
+        2 => 
+        array (
+          'grid' => 1,
+          'weight' => 0.66654854712970002,
+        ),
+      ),
+    );
+
     $layouts['landing-page'] = array(
-        'name' => __('Landing page', 'vantage'),    
-        'description' => __('Used on the homepage and other similar pages', 'vantage'),    // Optional
+        'name' => __('Landing page', 'proud'),    
+        'description' => __('Used on the homepage and other similar pages', 'proud'),    // Optional
         'widgets' => array (
           0 => 
           array (
@@ -332,6 +463,7 @@ class ProudSOPagebuilder {
           ),
         )
     );
+
     return $layouts;
   }  
 }
