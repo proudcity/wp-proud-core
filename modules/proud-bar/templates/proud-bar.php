09235dae7daf9853159597819c95523483fb79b9
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-01 01:58:57 -0800

    Proud bar updates

--- modules/proud-bar/templates/proud-bar.php
+++ modules/proud-bar/templates/proud-bar.php
@@ -1,13 +1,14 @@
 <div class="proudbar">
-  <a href="//proudcity.com" class="proudbar-logo" target="_blank">ProudCity</a>
-  <div class="proudbar-title"><a href="//proudcity.com/how-it-works" target="_blank" title="How ProudCity works"><?php print $stage ?></a></div>
+  <div class="proudbar-title pull-left"><a href="//proudcity.com/how-it-works" target="_blank" title="How ProudCity works"><?php print $stage ?></a></div>
   
-  <?php if ( 'example' !== $stage ): ?> 
-    <a href="https://insights.hotjar.com/s?siteId=124068&surveyId=6063" class="proudbar-btn">Feedback</a>
-    <a href="//proudcity.com/how-it-works" class="proudbar-btn proudbar-btn-circle" target="_blank" title="What is this?"><i class="fa fa-question"></i></a>
-    <a href="//proudcity.com/plans" class="proudbar-btn proudbar-btn-circle" target="_blank" title="Remove this"><i class="fa fa-times"></i></a>
+  <?php if ( 'example' === $stage ): ?> 
+    <a href="//proudcity.com" class="proudbar-logo pull-right" target="_blank">ProudCity</a>
+    Get started with ProudCity
+    <a href="//proudcity.com/start" class="proudbar-btn">Claim your city</a>  
   <?php else: ?>
-    <a href="//proudcity.com/start" class="proudbar-btn">Claim your city</a>
+    Welcome to our future website!
+    <a href="https://insights.hotjar.com/s?siteId=124068&surveyId=6063" class="proudbar-btn">Feedback</a>
+    <a href="//proudcity.com/plans" class="proudbar-btn proudbar-btn-circle" target="_blank" title="Remove this"><i class="fa fa-times"></i></a>
   <?php endif; ?>
 
 </div>
