3d4b7bb7172b55ad2f133a9af737c3afa84871ee
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-01 16:57:50 -0800

    Switching proudbar to typeform

--- modules/proud-bar/templates/proud-bar.php
+++ modules/proud-bar/templates/proud-bar.php
@@ -7,8 +7,9 @@
     <a href="//proudcity.com/start" class="proudbar-btn">Claim your city</a>  
   <?php else: ?>
     Welcome to our future website!
-    <a href="https://insights.hotjar.com/s?siteId=124068&surveyId=6063" class="proudbar-btn">Feedback</a>
+    <a href="https://proudcity.typeform.com/to/duZiun" class="proudbar-btn typeform-share" data-mode="2">Feedback</a>
     <!--<a href="//proudcity.com/plans" class="proudbar-btn proudbar-btn-circle" target="_blank" title="Remove this"><i class="fa fa-times"></i></a>-->
   <?php endif; ?>
 
 </div>
+<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>
