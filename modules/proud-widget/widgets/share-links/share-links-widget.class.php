7305e1e29cc1ca2a432562bfe4d05cf6b3f73b7d
Jeff Lyon <jeff@albatrossdigital.com>
2016-02-03 00:48:25 -0800

    Tweaking footer action hover text

--- modules/proud-widget/widgets/share-links/share-links-widget.class.php
+++ modules/proud-widget/widgets/share-links/share-links-widget.class.php
@@ -43,7 +43,7 @@ class ShareLinks extends Core\ProudWidget {
 
     ?>
     <!--<div class="dropdown share">-->
-      <a href="#" id="share-dropdown" data-toggle="dropdown"><i class="fa fa-share-alt"></i> Share <!--<span class="caret"></span>--></a>
+      <a href="#" id="share-dropdown" data-toggle="dropdown"><i class="fa fa-fw fa-share-alt"></i>Share <!--<span class="caret"></span>--></a>
       <ul class="dropdown-menu" aria-labelledby="share-dropdown">
         <li><a href="https://www.facebook.com/sharer/sharer.php" target="_blank"><i class="fa fa-facebook-square fa-fw"></i> Facebook</a></li>
         <li><a href="http://twitter.com/share"><i class="fa fa-twitter-square fa-fw"></i> Twitter</a></li>
