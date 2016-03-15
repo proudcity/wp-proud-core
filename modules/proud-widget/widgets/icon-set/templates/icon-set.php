96f70c1681d0d23bfd660ad0503808c13f0ff0a7
Alex Schmoe <aschmoe@gmail.com>
2016-03-15 12:08:02 -0700

    icon-button font size https://github.com/proudcity/wp-proudcity/issues/224

--- modules/proud-widget/widgets/icon-set/templates/icon-set.php
+++ modules/proud-widget/widgets/icon-set/templates/icon-set.php
@@ -3,7 +3,7 @@
     <?php if( !empty( $icon['link_title'] ) && !empty( $icon['link_url'] ) && !empty( $icon['fa_icon'] ) ): ?>
       <div class="card-wrap"><a href="<?php print $icon['link_url'] ?>" class="card text-center card-btn card-block">
         <i class="fa <?php print $icon['fa_icon'] ?> fa-3x"></i>
-        <h3><?php print $icon['link_title'] ?></h3>
+        <h4><?php print $icon['link_title'] ?></h4>
       </a></div><!--seperate-->
     <?php endif; ?>
   <?php endforeach; ?>
