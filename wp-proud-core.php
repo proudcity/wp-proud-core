80a018c5f04b37ee62ecca5990cf79067c7379ef
Jeff Lyon <jeff@albatrossdigital.com>
2016-03-19 16:20:46 -0700

    Updating license info in plugin  metadata

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,12 +3,11 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            1.0.0
+Version:            1.0
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
-License:            MIT License
-License URI:        http://opensource.org/licenses/MIT
+License:            Affero GPL v3
 */
 
 
