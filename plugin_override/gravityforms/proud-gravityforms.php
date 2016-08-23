e25676a9287962d744b707feb46a6b392796fc99
Alex Schmoe <aschmoe@gmail.com>
2016-08-22 17:04:24 -0700

    updating fontawesome version to latest, taking iconpicker off default, fixing gravity forms loading fontaweseome

--- plugin_override/gravityforms/proud-gravityforms.php
+++ plugin_override/gravityforms/proud-gravityforms.php
@@ -11,5 +11,11 @@ function gform_css_dequeue() {
   wp_deregister_style('gforms_datepicker_css');
   wp_dequeue_style('gforms_datepicker_css');
 }
-add_action( 'wp_footer', __NAMESPACE__ . '\\gform_css_dequeue', 5 );
-add_action( 'wp_print_styles', __NAMESPACE__ . '\\gform_css_dequeue', 5 );
+add_action( 'wp_footer', __NAMESPACE__ . '\\gform_css_dequeue', 100 );
+add_action( 'wp_print_styles', __NAMESPACE__ . '\\gform_css_dequeue', 100 );
+
+function gform_admin_css_dequeue() {
+  wp_deregister_style('gform_font_awesome');
+  wp_dequeue_style('gform_font_awesome');
+}
+add_action( 'admin_print_scripts', __NAMESPACE__ . '\\gform_admin_css_dequeue', 100 );
