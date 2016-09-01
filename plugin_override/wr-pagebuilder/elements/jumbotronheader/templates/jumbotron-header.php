8852551858b9632368ea55c3dd877b36ad7c07d6
Jeff Lyon <jeff@albatrossdigital.com>
2016-09-01 15:50:01 -0700

    Remove wr-pagebuilder overrides directory

--- plugin_override/wr-pagebuilder/elements/jumbotronheader/templates/jumbotron-header.php
+++
@@ -1,5 +0,0 @@
-<div class="jumbo-header"><div class="<?php print implode( ' ', $classes ) ?>" id="<?php print $random_id ?>" style="<?php print implode( '', $arr_styles ) ?>"<?php print $data_attr; ?>>
-  <div class="container"><div class="row"><div class="col-lg-5 col-md-8 col-sm-8">
-    <div class="jumbotron-bg"><?php print $content; ?></div>
-  </div></div></div>
-</div></div>
--- plugin_override/wr-pagebuilder/elements/jumbotronheader/templates/jumbotron-simple.php
+++
@@ -1,3 +0,0 @@
-<div class="<?php print implode( ' ', $classes ) ?>">
-  <div class="container"><?php print $content; ?></div>
-</div>
