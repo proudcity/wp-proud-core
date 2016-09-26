9da097fecd21cd9620e09593dbac6fcf9fbf86a6
Alex Schmoe <aschmoe@gmail.com>
2016-09-26 15:06:25 -0700

    adding height responsiveness to the slider menu

--- modules/proud-menu/assets/js/proud-menu.js
+++ modules/proud-menu/assets/js/proud-menu.js
@@ -1,22 +1,55 @@
 (function($, Proud) {
-  // Back button clicks
-  $('a[data-back-click]').on('click', function(event) {
-    event.preventDefault();
-    var level = $(this).data('back-click') || 1;
-    // Remove other active
-    $(this).parents('.menu-slider').removeClass(function (index, css) {
-        return (css.match (/level-[0-9]-active/g) || []).join(' ');
-    }).addClass('level-' + level + '-active');
+
+  $.fn.proudMenuSlider = (function() {
+      var self = this,
+          active = 1,
+          height = 0,
+          $slider = $(this);
+
+      self.init = function() {
+        // Set active
+        active = $slider.data('level-active');
+        // Set height
+        self.alterHeight(active, false);
+        // Init clicks
+        // @TODO evaluate if the navigation forward is important
+        $('[data-active-click]', $slider).on('click', self.sliderClick);
+        $('[data-back-click]', $slider).on('click', self.sliderClick);
+      }
+
+      self.sliderClick = function(event) {
+        event.preventDefault();
+        var level = $(this).data('active-click') || $(this).data('back-click') || 1;
+        // Alter items
+        self.alterHeight(level, true);
+        self.alterClass(level);
+        // Save active
+        active = level;
+      }
+
+      self.alterHeight = function(levelTo, animate) {
+        var toHeight = $slider.find('.level-' + levelTo).height();
+        if(!animate) {
+          $slider.height(toHeight);
+        }
+        else {
+          $slider.animate({
+            height: toHeight
+          }, 250 );
+        }
+        height = toHeight;
+      }
+
+      self.alterClass = function(level) {
+        $slider.removeClass(function (index, css) {
+            return (css.match (/level-[0-9]-active/g) || []).join(' ');
+        }).addClass('level-' + level + '-active');
+      }
+
+    return self.init();
   });
 
-  // Parent-to-child clicks
-  $('a[data-active-click]').on('click', function(event) {
-    event.preventDefault();
-    var level = $(this).data('active-click');
-    // Remove other active
-    $(this).parents('.menu-slider').removeClass(function (index, css) {
-        return (css.match (/level-[0-9]-active/g) || []).join(' ');
-    }).addClass('level-' + level + '-active');
-  });
+  // init menu sliders
+  $('.menu-slider').proudMenuSlider();
 
 })(jQuery, Proud);
--- modules/proud-menu/templates/menu-wrapper.php
+++ modules/proud-menu/templates/menu-wrapper.php
@@ -1,4 +1,4 @@
-<div class="menu-slider level-count-<?php echo count($menus) ?> level-<?php echo $active ?>-active">
+<div class="menu-slider level-count-<?php echo count($menus) ?> level-<?php echo $active ?>-active" data-level-active="<?php echo $active ?>">
   <div class="inner list-group">
     <?php echo implode('', $menus) ?> 
   </div>
