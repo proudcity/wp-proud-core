912e9e98302c33c15e17aa96dc36ea289e5c353a
Alex Schmoe <aschmoe@gmail.com>
2016-10-18 16:40:38 -0700

    adding titles for hover on navbar action buttons https://github.com/proudcity/wp-proudcity/issues/617

--- modules/proud-navbar/templates/nav-toolbar.php
+++ modules/proud-navbar/templates/nav-toolbar.php
@@ -1,11 +1,11 @@
 <div class="btn-toolbar pull-left" role="toolbar">
   <?php if( !get_option('proud_hide_toolbar_nav') ): ?>
-    <a data-proud-navbar="answers" href="#" class="btn navbar-btn faq-button"><i class="fa fa-question-circle"></i> Answers</a>
-    <a data-proud-navbar="payments" href="#" class="btn navbar-btn payments-button"><i class="fa fa-credit-card"></i> Payments</a>
+    <a title="Answers" data-proud-navbar="answers" href="#" class="btn navbar-btn faq-button"><i class="fa fa-question-circle"></i> Answers</a>
+    <a title="Payments" data-proud-navbar="payments" href="#" class="btn navbar-btn payments-button"><i class="fa fa-credit-card"></i> Payments</a>
     <?php if(get_option('311_service', 'link') !== 'link'): ?>
-      <a data-proud-navbar="report" href="#" class="btn navbar-btn issue-button"><i class="fa fa-wrench"></i> Issues</a>
+      <a title="Issues" data-proud-navbar="report" href="#" class="btn navbar-btn issue-button"><i class="fa fa-wrench"></i> Issues</a>
     <?php elseif(!empty(get_option('311_link_create'))): ?>
-      <a data-proud-navbar="report" data-click-external="true" href="<?php echo get_option('311_link_create') ?>" class="btn navbar-btn issue-button"><i class="fa fa-wrench"></i> Issues</a>
+      <a title="Issues" data-proud-navbar="report" data-click-external="true" href="<?php echo get_option('311_link_create') ?>" class="btn navbar-btn issue-button"><i class="fa fa-wrench"></i> Issues</a>
     <?php endif; ?>
   <?php endif; ?>
 </div>
@@ -13,5 +13,5 @@
   <a id="menu-button" href="#" class="btn navbar-btn menu-button"><span class="hamburger">
     <span>toggle menu</span>
   </span></a>
-  <a data-proud-navbar="search" href="#" class="btn navbar-btn search-btn"><i class="fa fa-search"></i> <span class="text sr-only">Search</span></a>
+  <a title="Search" data-proud-navbar="search" href="#" class="btn navbar-btn search-btn"><i class="fa fa-search"></i> <span class="text sr-only">Search</span></a>
 </div>
