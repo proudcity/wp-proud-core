d2d1d7e7da8a095efe5948232735e7d0d02c37a0
Jeff Lyon <jeff@albatrossdigital.com>
2016-11-02 16:29:10 -0700

    Add "Posted on" before news published date https://github.com/proudcity/wp-proudcity/issues/649

--- modules/proud-teasers/templates/teaser-list.php
+++ modules/proud-teasers/templates/teaser-list.php
@@ -7,7 +7,7 @@
     <?php endif; ?>
     <div class="col-md-9 pull-left">
       <?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h4>' ); ?>
-      <p class="muted"><?php echo get_the_date(); ?></p>
+      <p class="muted"><?php echo __('Posted on', 'wp-proud-core') ?> <?php echo get_the_date(); ?></p>
       <?php echo \Proud\Core\wp_trim_excerpt(); ?>
     </div>
   </div>
