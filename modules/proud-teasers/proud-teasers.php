8d0bd152dabc4ab8d2defce600a8ba754bdfe81d
Alex Schmoe <aschmoe@gmail.com>
2016-12-07 13:56:04 -0800

    adding filters, options to allow for elastic search

--- modules/proud-teasers/proud-teasers.php
+++ modules/proud-teasers/proud-teasers.php
@@ -133,7 +133,12 @@ if ( !class_exists( 'TeaserList' ) ) {
       // If posts per page is set to 0, make it show all posts (up to 100)
       $args[ 'posts_per_page' ] = $args[ 'posts_per_page' ] == 0 ? 100 : $args[ 'posts_per_page' ];
       // Build query
-      $this->query = new \WP_Query( apply_filters( 'proud_teaser_query_args', $args, $this->post_type ) );
+      $this->query = new \WP_Query( apply_filters( 
+        'proud_teaser_query_args', 
+        $args,
+        !empty( $args['s'] ) ? $args['s'] : null,
+        $this->post_type 
+      ) );
 
       // Alter pagination links to deal with issues with documents, ext
       add_filter('get_pagenum_link', [$this, 'alter_pagination_path']);
--- modules/proud-teasers/templates/teaser-search.php
+++ modules/proud-teasers/templates/teaser-search.php
@@ -3,7 +3,7 @@
     <div class="col-md-12">
       <div class="search-title">
         <h4 class="entry-title margin-top-large"><?php echo $proudsearch->get_post_link($post) ?></h4>
-        <i class="fa fa-2x <?php echo $search_meta['icon'] ?>"></i>
+        <i class="fa <?php echo $search_meta['icon'] ?>"></i>
       </div>
       <p><?php echo \Proud\Core\wp_trim_excerpt( '', false, true ); ?></p>
       <?php echo $proudsearch->get_post_link($post, 'See more') ?>
