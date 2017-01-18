0752a103c3a5bc2f628fb12bb629d02d78baf22c
Alex Schmoe <aschmoe@gmail.com>
2017-01-18 12:35:57 -0800

    adding a url param to the twitter sharer

--- modules/proud-widget/widgets/share-links/share-links-widget.class.php
+++ modules/proud-widget/widgets/share-links/share-links-widget.class.php
@@ -61,7 +61,7 @@ class ShareLinks extends Core\ProudWidget {
       <a<?php if( !empty( $instance['classes'] ) ) { echo ' class="' . $instance['classes'] .'"'; } ?> href="#" id="share-dropdown" data-toggle="dropdown"><i class="fa fa-fw fa-share-alt"></i>Share <!--<span class="caret"></span>--></a>
       <ul class="dropdown-menu" aria-labelledby="share-dropdown">
         <li><a href="https://www.facebook.com/sharer/sharer.php" target="_blank"><i class="fa fa-facebook-square fa-fw"></i> Facebook</a></li>
-        <li><a href="http://twitter.com/share"><i class="fa fa-twitter-square fa-fw"></i> Twitter</a></li>
+        <li><a href="http://twitter.com/share?url=<?php print urlencode($url); ?>"><i class="fa fa-twitter-square fa-fw"></i> Twitter</a></li>
         <li><a href="mailto:?subject=<?php print urlencode($title); ?>&body=Read more: <?php print urlencode($url); ?>"><i class="fa fa-envelope fa-fw"></i> Email</a>
       </ul>
     <!--</div>-->
