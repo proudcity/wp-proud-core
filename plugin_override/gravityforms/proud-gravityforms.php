5919f42fcbd3160be636c1fa69c424c19841e9fc
Alex Schmoe <aschmoe@gmail.com>
2017-02-01 14:19:59 -0800

    CSS Gravityforms date picker, time field https://github.com/proudcity/wp-proudcity/issues/750

--- plugin_override/gravityforms/proud-gravityforms.php
+++ plugin_override/gravityforms/proud-gravityforms.php
@@ -17,8 +17,7 @@ function gform_css_dequeue() {
   wp_deregister_style('gforms_datepicker_css');
   wp_dequeue_style('gforms_datepicker_css');
 }
-add_action( 'wp_footer', __NAMESPACE__ . '\\gform_css_dequeue', 100 );
-add_action( 'wp_print_styles', __NAMESPACE__ . '\\gform_css_dequeue', 100 );
+add_action( 'gform_enqueue_scripts', __NAMESPACE__ . '\\gform_css_dequeue', 100 );
 
 function gform_admin_css_dequeue() {
   wp_deregister_style('gform_font_awesome');
