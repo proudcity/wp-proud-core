f4828c6e6bd3cea8f66e8191cb285b538470b412
Alex Schmoe <aschmoe@gmail.com>
2017-03-16 12:36:40 -0700

    fixing php warning, Card styles on xs-2 misses sm media-query size https://github.com/proudcity/wp-proudcity/issues/896

--- modules/proud-widget/widgets/icon-set/templates/icon-set.php
+++ modules/proud-widget/widgets/icon-set/templates/icon-set.php
@@ -1,7 +1,7 @@
-<div class="card-columns card-columns-xs-2 card-columns-md-<?php echo $md_col ?>  card-columns-equalize">
+<div class="card-columns card-columns-xs-2 card-columns-sm-<?php echo ($md_col - 1) > 1 ? $md_col - 1 : 2  ?> card-columns-md-<?php echo $md_col ?>  card-columns-equalize">
   <?php foreach ( $iconset as $icon ) : ?>
     <?php if( !empty( $icon['link_title'] ) && !empty( $icon['link_url'] ) && !empty( $icon['fa_icon'] ) ): ?>
-      <div class="card-wrap"><a href="<?php print $icon['link_url'] ?>" class="card text-center card-btn card-block" <?php if($icon['external']): ?>target="_blank"<?php endif;?> >
+      <div class="card-wrap"><a href="<?php print $icon['link_url'] ?>" class="card text-center card-btn card-block" <?php if( !empty( $icon['external'] ) ): ?>target="_blank"<?php endif;?> >
         <i class="fa <?php print $icon['fa_icon'] ?> fa-3x"></i>
         <h4><?php print $icon['link_title'] ?></h4>
       </a></div><!--seperate-->
