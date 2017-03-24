f4e31db779beb1a6a72e751b8d3c8785175e8c19
Alex Schmoe <aschmoe@gmail.com>
2017-03-24 11:22:24 -0700

    Kettering: Allow jobs taxonomy to be multi-slect https://github.com/proudcity/wp-proudcity/issues/936

--- plugin_override/wp-job-manager/proud-wp-job-manager.php
+++ plugin_override/wp-job-manager/proud-wp-job-manager.php
@@ -10,3 +10,27 @@ function proud_wp_job_manger_css_dequeue() {
   wp_dequeue_style('wp-job-manager-frontend');
 }
 add_action( 'wp_print_styles', __NAMESPACE__ . '\\proud_wp_job_manger_css_dequeue', 100 );
+
+
+/**
+ * Prints out list of job types for multi / single jobs
+ */
+function proud_wp_job_manager_print_types($post) {
+  if(!$post) {
+    global $post;
+  }
+  if(job_manager_multi_job_type()): ?>
+    <?php 
+      $types = wp_get_post_terms( $post->ID, 'job_listing_type' );
+      foreach ($types as $type) {
+        ?>
+          <span class="label job-type <?php echo sanitize_title( $type->slug ) ?>">
+            <?php echo $type->name ?>
+          </span>
+        <?php
+      }
+    ?>
+  <?php else: ?>
+    <span class="label job-type <?php echo get_the_job_type() ? sanitize_title( get_the_job_type()->slug ) : ''; ?>"><?php the_job_type(); ?></span>
+  <?php endif;
+}
