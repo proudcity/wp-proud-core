b2b85553ad6c6845c572b0cd659742bc5e17a16a
Jeff Lyon <jeff@albatrossdigital.com>
2017-06-30 12:07:56 -0700

    Adding gravityformsstripe plugin overrides for ProudCity Payments

---
+++ plugin_override/gravityformsstripe/proud-gravityformsstripe.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Proud\GravityformsStripe;
+
+class ProudGravityformsStripe {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        add_filter('gform_stripe_charge_pre_create', [ $this, 'gform_stripe_charge_pre_create' ], 10, 5);
+    }
+
+
+    function gform_stripe_charge_pre_create($charge_meta, $feed, $submission_data, $form, $entry) {
+        $account = get_option('proudcity_payments_account', false);
+        if ($account) {
+            $charge_meta['destination'] = $account;
+            $charge_meta['transfer_group'] = $form['title'];
+        }
+
+        // Metadata
+        //$charge_meta['description'] = $form['title'];
+        $charge_meta['metadata']['form_title'] = $form['title'];
+        $charge_meta['metadata']['form_id'] = $form['id'];
+
+        // Add the ProudCity Payments fee
+        $percent = getenv('PROUDCITY_PAYMENTS_PERCENT') ? parseFloat(getenv('PROUDCITY_PAYMENTS_PERCENT')) : 3;
+        $charge_meta['application_fee'] = round(30 + $submission_data['payment_amount'] * $percent); // In cents
+
+        return $charge_meta;
+
+        print_R($submission_data);
+        print_r($feed);
+        print_r($entry);
+        print_r($form);
+        print_r($charge_meta);
+        exit;
+    }
+
+
+}
+
+new ProudGravityformsStripe;
