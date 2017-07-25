98b3970737c0801e13d16e1300ab0fe5a6c8b4c2
Jeff Lyon <jeff@albatrossdigital.com>
2017-07-25 02:03:56 -0700

    Adding hooks implementations for Payments: Subscriptions https://github.com/proudcity/wp-proudcity/issues/1052

--- plugin_override/gravityformsstripe/proud-gravityformsstripe.php
+++ plugin_override/gravityformsstripe/proud-gravityformsstripe.php
@@ -9,6 +9,12 @@ class ProudGravityformsStripe {
      */
     public function __construct() {
         add_filter('gform_stripe_charge_pre_create', [ $this, 'gform_stripe_charge_pre_create' ], 10, 5);
+
+        add_filter('gform_stripe_create_customer', [$this, 'gform_stripe_create_customer'], 10, 1);
+        add_filter('gform_stripe_update_subscription', [$this, 'gform_stripe_update_subscription'], 10, 2);
+
+        add_filter('gform_stripe_subscription_single_payment_amount', [$this, 'gform_stripe_subscription_single_payment_amount'], 10, 4);
+        add_filter('gform_stripe_subscription_trial_period_days', [$this, 'gform_stripe_subscription_trial_period_days'], 10, 3);
     }
 
 
@@ -41,6 +47,56 @@ class ProudGravityformsStripe {
         //print_r($charge_meta);
         //exit;
     }
+
+
+    function gform_stripe_create_customer($customer_meta) {
+
+        $account = get_option('proudcity_payments_account', false);
+        $customer = \Stripe\Customer::create( $customer_meta, ['stripe_account' => $account] );
+        //print_r($customer);
+        return $customer;
+
+    }
+
+    function gform_stripe_update_subscription($customer, $plan) {
+
+        $account = get_option('proudcity_payments_account', false);
+        $subscription = $customer->updateSubscription( array( 'plan' => $plan->id ), ['stripe_account' => $account] );
+        //print_R($subscription);
+        return $subscription;
+
+    }
+
+    function gform_stripe_subscription_trial_period_days($trial_period_days, $form, $submission_data) {
+
+        $day = get_option('proudcity_payments_recurring_date', false);
+
+        if ($day) {
+            $curMonth = date('n');
+            $curYear  = date('Y');
+
+            if ($curMonth == 12)
+                $firstDayNextMonth = mktime(0, 0, 0, 0, $day, $curYear+1);
+            else
+                $firstDayNextMonth = mktime(0, 0, 0, $curMonth+1, $day);
+
+            $daysTilNextMonth = ($firstDayNextMonth - mktime()) / (24 * 3600);
+
+            return ceil($daysTilNextMonth);
+        }
+
+        return $trial_period_days;
+
+    }
+
+    function gform_stripe_subscription_single_payment_amount($single_payment_amount, $payment_amount, $form, $submission_data) {
+
+        // @todo
+        return $single_payment_amount;
+
+    }
+
+
 
 
 }
