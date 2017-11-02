8ac428d56d764faa80f34e62b24e374417a7f911
Alex Schmoe <aschmoe@gmail.com>
2017-11-02 13:03:04 -0700

    fixing class for skiplinks

--- modules/proud-accessibility/templates/skip-links.php
+++ modules/proud-accessibility/templates/skip-links.php
@@ -1,5 +1,5 @@
-<ul id="skiplinks" role="navigation" aria-label="Skip links">
-  <li><a class="sr-only" href="#content-main">Skip to content</a></li>
-  <li><a class="sr-only" href="#navbar-external">Skip to navigation</a></li>
-  <li><a class="sr-only" href="/sitemap">Site map</a></li>
+<ul id="skiplinks" role="navigation" aria-label="Skip links" class="sr-only">
+  <li><a href="#content-main">Skip to content</a></li>
+  <li><a href="#navbar-external">Skip to navigation</a></li>
+  <li><a href="/sitemap">Site map</a></li>
 </ul>
