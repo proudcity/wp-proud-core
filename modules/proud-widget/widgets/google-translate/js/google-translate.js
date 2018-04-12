1e37f4683878188c82978748016df8ee5720a17e
Alex Schmoe <aschmoe@gmail.com>
2018-04-12 02:05:12 -0700

    accessibility edits

--- modules/proud-widget/widgets/google-translate/js/google-translate.js
+++ modules/proud-widget/widgets/google-translate/js/google-translate.js
@@ -3,4 +3,5 @@ function googleTranslateElementInit() {
   new google.translate.TranslateElement({
     pageLanguage: 'en',
   }, 'google_translate_element');
+  document.getElementsByClassName('goog-te-combo')[0].setAttribute('aria-labelledby', 'google-translate-label');
 }
