82f810c9958c2f88f09d0bb382aac22901260037
Alex Schmoe <aschmoe@gmail.com>
2018-12-04 01:26:00 -0800

    fixing bad tag in navbar

--- modules/proud-navbar/templates/nav-toolbar.php
+++ modules/proud-navbar/templates/nav-toolbar.php
@@ -9,4 +9,4 @@
 </ul>
 <ul class="btn-toolbar pull-right list-unstyled clearfix" role="toolbar">
   <li><a title="<?php echo $search_button['title'] ?>"<?php if( $search_button['data_key'] ) : ?> data-proud-navbar="<?php echo $search_button['data_key'] ?>"<?php endif; ?><?php echo $search_button['data_attrs'] ?> href="<?php echo $search_button['href'] ?>" class="<?php echo $search_button['classes'] ?>"><i aria-hidden="true" class="fa <?php echo $search_button['icon'] ?>"></i> <?php echo $search_button['title'] ?></a></li>
-</div>
+</ul>
