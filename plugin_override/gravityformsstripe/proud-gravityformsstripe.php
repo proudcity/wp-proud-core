124d2ba7b8f5a969dac42eb47a2d5673e889cc4a
Jeff Lyon <jeff@albatrossdigital.com>
2020-02-13 21:16:46 -0800

    Stripe pluginoverride changes

--- plugin_override/gravityformsstripe/proud-gravityformsstripe.php
+++ plugin_override/gravityformsstripe/proud-gravityformsstripe.php
@@ -8,17 +8,30 @@ class ProudGravityformsStripe {
      * Constructor
      */
     public function __construct() {
+        add_filter('gform_stripe_post_include_api', [ $this, 'gform_stripe_post_include_api' ], 10, 5);
         add_filter('gform_stripe_charge_pre_create', [ $this, 'gform_stripe_charge_pre_create' ], 10, 5);
 
-        add_filter('gform_stripe_create_customer', [$this, 'gform_stripe_create_customer'], 10, 1);
-        add_filter('gform_stripe_create_plan', [$this, 'gform_stripe_create_plan'], 10, 1);
-        add_filter('gform_stripe_get_plan', [$this, 'gform_stripe_get_plan'], 10, 1);
-        add_filter('gform_stripe_update_subscription', [$this, 'gform_stripe_update_subscription'], 10, 2);
+        
+        // add_filter('gform_stripe_create_customer', [$this, 'gform_stripe_create_customer'], 10, 1);
+        // add_filter('gform_stripe_create_plan', [$this, 'gform_stripe_create_plan'], 10, 1);
+        // add_filter('gform_stripe_get_plan', [$this, 'gform_stripe_get_plan'], 10, 1);
+        // add_filter('gform_stripe_update_subscription', [$this, 'gform_stripe_update_subscription'], 10, 2);
 
-        add_filter('gform_stripe_subscription_single_payment_amount', [$this, 'gform_stripe_subscription_single_payment_amount'], 10, 4);
-        add_filter('gform_stripe_subscription_trial_period_days', [$this, 'gform_stripe_subscription_trial_period_days'], 10, 3);
-        add_filter('gform_stripe_post_create_subscription', [$this, 'gform_stripe_post_create_subscription'], 10, 2);
+        // add_filter('gform_stripe_subscription_single_payment_amount', [$this, 'gform_stripe_subscription_single_payment_amount'], 10, 4);
+        // add_filter('gform_stripe_subscription_trial_period_days', [$this, 'gform_stripe_subscription_trial_period_days'], 10, 3);
+        // add_filter('gform_stripe_post_create_subscription', [$this, 'gform_stripe_post_create_subscription'], 10, 2);
 
+    }
+
+    /**
+     * We override the secret from GF settings, with the PC master secret, typically passed from ENV vars.
+     *
+     * @return void
+     */
+    function gform_stripe_post_include_api() {
+        $secret = get_option('proudcity_payments_secret', false);
+        $secret = !empty($secret) ? $secret : getenv('PROUDCITY_PAYMENTS_SECRET');
+        \Stripe\Stripe::setApiKey( $secret );
     }
 
 
@@ -27,20 +40,19 @@ class ProudGravityformsStripe {
 
         // Stripe Connect stuff
         if ($account) {
-            // Set up Stripe Connect destination
-            $charge_meta['destination'] = $account;
-            $charge_meta['transfer_group'] = $form['title'];
-
             // Add the ProudCity Payments fee
             $percent = getenv('PROUDCITY_PAYMENTS_PERCENT') ? (float)getenv('PROUDCITY_PAYMENTS_PERCENT') : 3;
-            $charge_meta['application_fee'] = round(30 + $submission_data['payment_amount'] * $percent); // In cents
+            $charge_meta['application_fee_amount'] = round(30 + $submission_data['payment_amount'] * $percent); // In cents
+
+            // Set up Stripe Connect destination
+            $charge_meta['transfer_data']['destination'] = $account;
+            $charge_meta['transfer_group'] = $form['title'];
 
             // Add the statement descriptor suffix
-            // Will be in the form `ProudCity * $descriptor` (can be22 characters total)
+            // Will be in the form `ProudCity * $descriptor` (can be 22 characters total)
             // Example: `ProudCity * San Rafael` (22 chars)
             // https://stripe.com/docs/statement-descriptors
             $descriptor = get_option('proudcity_payments_descriptor', get_bloginfo('name'));
-            $charge_meta['statement_descriptor'] = $descriptor;
             $charge_meta['statement_descriptor_suffix'] = $descriptor;
         }
 
@@ -49,6 +61,8 @@ class ProudGravityformsStripe {
         $charge_meta['metadata']['form_title'] = $form['title'];
         $charge_meta['metadata']['form_id'] = $form['id'];
         $charge_meta['metadata']['entry_id'] = $entry['id'];
+
+        // print_r($charge_meta);exit;
 
         return $charge_meta;
 
@@ -61,140 +75,140 @@ class ProudGravityformsStripe {
     }
 
 
-    function gform_stripe_create_customer($customer_meta) {
+    // function gform_stripe_create_customer($customer_meta) {
 
-        $account = get_option('proudcity_payments_account', false);
-        $customer = \Stripe\Customer::create( $customer_meta, ['stripe_account' => $account] );
-        //print_r($customer);
-        return $customer;
+    //     $account = get_option('proudcity_payments_account', false);
+    //     $customer = \Stripe\Customer::create( $customer_meta, ['stripe_account' => $account] );
+    //     //print_r($customer);
+    //     return $customer;
 
-    }
+    // }
 
-    function gform_stripe_create_plan($plan_meta) {
+    // function gform_stripe_create_plan($plan_meta) {
 
-        $account = get_option('proudcity_payments_account', false);
-        $plan = \Stripe\Plan::create( $plan_meta, ['stripe_account' => $account] );
-        return $plan;
+    //     $account = get_option('proudcity_payments_account', false);
+    //     $plan = \Stripe\Plan::create( $plan_meta, ['stripe_account' => $account] );
+    //     return $plan;
 
-    }
+    // }
 
-    function gform_stripe_get_plan($plan_id) {
+    // function gform_stripe_get_plan($plan_id) {
 
-        try {
+    //     try {
 
-            // Get Stripe plan.
-            $account = get_option('proudcity_payments_account', false);
-            $plan = \Stripe\Plan::retrieve( $plan_id, ['stripe_account' => $account] );
+    //         // Get Stripe plan.
+    //         $account = get_option('proudcity_payments_account', false);
+    //         $plan = \Stripe\Plan::retrieve( $plan_id, ['stripe_account' => $account] );
 
-        } catch ( \Exception $e ) {
+    //     } catch ( \Exception $e ) {
 
-            /**
-             * There is no error type specific to failing to retrieve a subscription when an invalid plan ID is passed. We assume here
-             * that any 'invalid_request_error' means that the subscription does not exist even though other errors (like providing
-             * incorrect API keys) will also generate the 'invalid_request_error'. There is no way to differentiate these requests
-             * without relying on the error message which is more likely to change and not reliable.
-             */
+    //         /**
+    //          * There is no error type specific to failing to retrieve a subscription when an invalid plan ID is passed. We assume here
+    //          * that any 'invalid_request_error' means that the subscription does not exist even though other errors (like providing
+    //          * incorrect API keys) will also generate the 'invalid_request_error'. There is no way to differentiate these requests
+    //          * without relying on the error message which is more likely to change and not reliable.
+    //          */
 
-            // Get error response.
-            $response = $e->getJsonBody();
+    //         // Get error response.
+    //         $response = $e->getJsonBody();
 
-            // If error is an invalid request error, return error message.
-            if ( rgars( $response, 'error/type' ) !== 'invalid_request_error' ) {
-                $plan = $this->authorization_error( $e->getMessage() );
-            } else {
-                $plan = false;
-            }
+    //         // If error is an invalid request error, return error message.
+    //         if ( rgars( $response, 'error/type' ) !== 'invalid_request_error' ) {
+    //             $plan = $this->authorization_error( $e->getMessage() );
+    //         } else {
+    //             $plan = false;
+    //         }
 
-        }
+    //     }
 
-        return $plan;
+    //     return $plan;
 
-    }
-
-
-    function gform_stripe_update_subscription($customer, $plan) {
-
-        $account = get_option('proudcity_payments_account', false);
-        $subscription = $customer->updateSubscription( array( 'plan' => $plan->id ), ['stripe_account' => $account] );
-
-        return $subscription;
-
-    }
-
-    function gform_stripe_subscription_trial_period_days($trial_period_days, $form, $submission_data) {
-
-        $day = get_option('proudcity_payments_recurring_date', false);
-        $dayDouble = get_option('proudcity_payments_recurring_date_double', false);
-
-        if ($day) {
-
-            // The ProudCity Stripe account is set the America/Los_Angeles
-            date_default_timezone_set('America/Los_Angeles');
-            $date = time();
-            $curDay = (int) date('d', $date);
-            $curMonth = (int) date('n', $date);
-            $curYear  = (int) date('Y', $date);
-
-            // No trial necessary
-            if ($curDay <= $day) {
-                return $day - $curDay;
-            }
-
-            // Trial logic for $dayDouble is done in gform_stripe_post_create_subscription() below
+    // }
 
 
-            // We need to trial until $day next month
-            if ($curMonth == 12) {
-                $firstDayNextMonth = mktime(0, 0, 0, 1, $day, $curYear+1);
-            }
-            else {
-                $firstDayNextMonth = mktime(0, 0, 0, $curMonth+1, $day, $curYear);
-            }
+    // function gform_stripe_update_subscription($customer, $plan) {
 
-            $daysTilNextMonth = ($firstDayNextMonth - mktime(0, 0, 0, $curMonth, $curDay, $curYear)) / (24 * 3600);
+    //     $account = get_option('proudcity_payments_account', false);
+    //     $subscription = $customer->updateSubscription( array( 'plan' => $plan->id ), ['stripe_account' => $account] );
 
-            $daysTilNextMonth = ceil($daysTilNextMonth) - 1;
-            return $daysTilNextMonth;
-        }
+    //     return $subscription;
 
-        return $trial_period_days;
+    // }
 
-    }
+    // function gform_stripe_subscription_trial_period_days($trial_period_days, $form, $submission_data) {
+
+    //     $day = get_option('proudcity_payments_recurring_date', false);
+    //     $dayDouble = get_option('proudcity_payments_recurring_date_double', false);
+
+    //     if ($day) {
+
+    //         // The ProudCity Stripe account is set the America/Los_Angeles
+    //         date_default_timezone_set('America/Los_Angeles');
+    //         $date = time();
+    //         $curDay = (int) date('d', $date);
+    //         $curMonth = (int) date('n', $date);
+    //         $curYear  = (int) date('Y', $date);
+
+    //         // No trial necessary
+    //         if ($curDay <= $day) {
+    //             return $day - $curDay;
+    //         }
+
+    //         // Trial logic for $dayDouble is done in gform_stripe_post_create_subscription() below
 
 
+    //         // We need to trial until $day next month
+    //         if ($curMonth == 12) {
+    //             $firstDayNextMonth = mktime(0, 0, 0, 1, $day, $curYear+1);
+    //         }
+    //         else {
+    //             $firstDayNextMonth = mktime(0, 0, 0, $curMonth+1, $day, $curYear);
+    //         }
 
-    function gform_stripe_post_create_subscription($customer, $plan) {
+    //         $daysTilNextMonth = ($firstDayNextMonth - mktime(0, 0, 0, $curMonth, $curDay, $curYear)) / (24 * 3600);
 
-        $account = get_option('proudcity_payments_account', false);
-        $day = get_option('proudcity_payments_recurring_date', false);
-        $dayDouble = get_option('proudcity_payments_recurring_date_double', false);
+    //         $daysTilNextMonth = ceil($daysTilNextMonth) - 1;
+    //         return $daysTilNextMonth;
+    //     }
 
-        // The ProudCity Stripe account is set the America/Los_Angeles
-        date_default_timezone_set('America/Los_Angeles');
-        $date = time();
-        $curDay = (int)date('d', $date);
-        $curMonth = (int)date('n', $date);
-        $curYear = (int)date('Y', $date);
+    //     return $trial_period_days;
 
-        if ($curDay > $day && $curDay <= $dayDouble) {
-            $charge_meta = [
-                "customer" => $customer->id,
-                "amount" => $plan->amount,
-                "currency" => "usd",
-                "description" => date('F', $date) . ': ' . $plan->name,
-            ];
-            $charge = \Stripe\Charge::create($charge_meta, ['stripe_account' => $account]);
-        }
-    }
+    // }
 
 
 
-    function gform_stripe_subscription_single_payment_amount($single_payment_amount, $payment_amount, $form, $submission_data) {
+    // function gform_stripe_post_create_subscription($customer, $plan) {
 
-        // @todo
-        return $single_payment_amount;
+    //     $account = get_option('proudcity_payments_account', false);
+    //     $day = get_option('proudcity_payments_recurring_date', false);
+    //     $dayDouble = get_option('proudcity_payments_recurring_date_double', false);
 
-    }
+    //     // The ProudCity Stripe account is set the America/Los_Angeles
+    //     date_default_timezone_set('America/Los_Angeles');
+    //     $date = time();
+    //     $curDay = (int)date('d', $date);
+    //     $curMonth = (int)date('n', $date);
+    //     $curYear = (int)date('Y', $date);
+
+    //     if ($curDay > $day && $curDay <= $dayDouble) {
+    //         $charge_meta = [
+    //             "customer" => $customer->id,
+    //             "amount" => $plan->amount,
+    //             "currency" => "usd",
+    //             "description" => date('F', $date) . ': ' . $plan->name,
+    //         ];
+    //         $charge = \Stripe\Charge::create($charge_meta, ['stripe_account' => $account]);
+    //     }
+    // }
+
+
+
+    // function gform_stripe_subscription_single_payment_amount($single_payment_amount, $payment_amount, $form, $submission_data) {
+
+    //     // @todo
+    //     return $single_payment_amount;
+
+    // }
 
 
 
