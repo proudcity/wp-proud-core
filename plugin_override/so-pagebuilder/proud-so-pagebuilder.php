829dba1cddb6029a0daee0cbae7156144dc614a7
Jeff Lyon <jeff@albatrossdigital.com>
2020-04-04 16:26:17 -0700

    /connect page layout #1672

--- plugin_override/so-pagebuilder/proud-so-pagebuilder.php
+++ plugin_override/so-pagebuilder/proud-so-pagebuilder.php
@@ -172,7 +172,7 @@ class ProudSOPagebuilder {
 
     // To generate these exports, follow steps on https://siteorigin.com/thread/programmatically-define-prebuilt-page-layouts/
     // Or, just enable Debug Bar and Debug Bar Consolr, and enter
-    //   <pre><?php var_export( get_post_meta(10118, 'panels_data', true));
+    //   <pre><?php var_export( get_post_meta(30746, 'panels_data', true));
 	function proud_prebuilt_layouts( $layouts ) {
 
 		$layouts['agency-page'] = array(
@@ -728,6 +728,250 @@ class ProudSOPagebuilder {
 							'weight' => 1,
 						),
 				)
+        );
+        
+
+        $layouts['connect-page'] = array(
+			'name'        => __( 'Connect page', 'proud' ),
+			'description' => __( 'Links to Social networks and Facebook and Twitter embeds', 'proud' ),    // Optional
+            'widgets' => 
+            array (
+                0 => 
+                array (
+                'headertype' => 'simple',
+                'text' => '<h1>Connect</h1><p class="lead">Connect with us on social media.</p>',
+                'background' => 'none',
+                'pattern' => '',
+                'repeat' => 'full',
+                'featured_image' => 'no',
+                'image' => '',
+                'slideshow' => 
+                array (
+                    0 => 
+                    array (
+                    'slide_title' => '',
+                    'description' => '',
+                    'link_title' => '',
+                    'link_url' => '',
+                    'slide_image' => '',
+                    'weight' => '0',
+                    ),
+                ),
+                'random' => 
+                array (
+                    0 => 
+                    array (
+                    'random_image' => '',
+                    'weight' => '0',
+                    ),
+                ),
+                'image_vertical' => 'middle',
+                'box_position' => 'middle_left',
+                'make_inverse' => 'yes',
+                'so_sidebar_emulator_id' => 'proud_jumbotron_header-358410000',
+                'option_name' => 'widget_proud_jumbotron_header',
+                'panels_info' => 
+                array (
+                    'class' => 'JumbotronHeader',
+                    'raw' => false,
+                    'grid' => 0,
+                    'cell' => 0,
+                    'id' => 0,
+                    'widget_id' => 'c54afaeb-eedc-4853-8eae-60dbf1e6838a',
+                    'style' => 
+                    array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    ),
+                ),
+                ),
+                1 => 
+                array (
+                'title' => '',
+                'iconset' => 
+                array (
+                    0 => 
+                    array (
+                    'link_title' => 'Facebook',
+                    'link_url' => 'https://facebook.com/getproudcity',
+                    'fa_icon' => 'fab fa-facebook-f',
+                    'external' => '0',
+                    'weight' => '0',
+                    ),
+                    1 => 
+                    array (
+                    'link_title' => 'Twitter',
+                    'link_url' => 'https://twitter.com/getproudcity',
+                    'fa_icon' => 'fab fa-twitter',
+                    'external' => '0',
+                    'weight' => '1',
+                    ),
+                    2 => 
+                    array (
+                    'link_title' => 'LinkedIn',
+                    'link_url' => 'https://www.linkedin.com/company/7321596',
+                    'fa_icon' => 'fab fa-linkedin-in',
+                    'external' => '0',
+                    'weight' => '2',
+                    ),
+                    3 => 
+                    array (
+                    'link_title' => 'Instagram',
+                    'link_url' => 'https://www.instagram.com/getproudcity/',
+                    'fa_icon' => 'fab fa-instagram',
+                    'external' => '0',
+                    'weight' => '3',
+                    ),
+                    4 => 
+                    array (
+                    'link_title' => 'YouTube',
+                    'link_url' => 'https://www.youtube.com/channel/UCVnqaioIx4qPbr5JWE_m1yQ',
+                    'fa_icon' => 'fab fa-youtube',
+                    'external' => '0',
+                    'weight' => '4',
+                    ),
+                ),
+                'so_sidebar_emulator_id' => 'proud_icon_set-358410001',
+                'option_name' => 'widget_proud_icon_set',
+                'panels_info' => 
+                array (
+                    'class' => 'IconSet',
+                    'raw' => false,
+                    'grid' => 1,
+                    'cell' => 0,
+                    'id' => 1,
+                    'widget_id' => 'de8814b0-9e88-487d-8838-53786dc227dc',
+                    'style' => 
+                    array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    ),
+                ),
+                ),
+                2 => 
+                array (
+                'facebook_page_url' => 'https://www.facebook.com/getproudcity',
+                'tabs' => 
+                array (
+                    'timeline' => 'timeline',
+                ),
+                'height' => '2000',
+                'so_sidebar_emulator_id' => 'social_facebook-358410003',
+                'option_name' => 'widget_social_facebook',
+                'panels_info' => 
+                array (
+                    'class' => 'SocialFacebook',
+                    'raw' => false,
+                    'grid' => 2,
+                    'cell' => 0,
+                    'id' => 2,
+                    'widget_id' => '133ab78f-3b83-4d9d-b6fc-ed22a3cea748',
+                    'style' => 
+                    array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    ),
+                ),
+                ),
+                3 => 
+                array (
+                'handle' => '@getproudcity',
+                'height' => '2000',
+                'so_sidebar_emulator_id' => 'social_twitter-358410005',
+                'option_name' => 'widget_social_twitter',
+                'panels_info' => 
+                array (
+                    'class' => 'SocialTwitter',
+                    'raw' => false,
+                    'grid' => 2,
+                    'cell' => 1,
+                    'id' => 3,
+                    'widget_id' => 'ca38719a-5817-4fca-828f-991fd2427938',
+                    'style' => 
+                    array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    ),
+                ),
+                ),
+            ),
+            'grids' => 
+            array (
+                0 => 
+                array (
+                'cells' => 1,
+                'style' => 
+                array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    'row_stretch' => 'full',
+                    'cell_alignment' => 'flex-start',
+                ),
+                'ratio' => 1,
+                'ratio_direction' => 'right',
+                ),
+                1 => 
+                array (
+                'cells' => 1,
+                'style' => 
+                array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    'cell_alignment' => 'flex-start',
+                ),
+                'ratio' => 1,
+                'ratio_direction' => 'right',
+                ),
+                2 => 
+                array (
+                'cells' => 2,
+                'style' => 
+                array (
+                    'background_image_attachment' => false,
+                    'background_display' => 'tile',
+                    'cell_alignment' => 'flex-start',
+                ),
+                ),
+            ),
+            'grid_cells' => 
+            array (
+                0 => 
+                array (
+                'grid' => 0,
+                'index' => 0,
+                'weight' => 1,
+                'style' => 
+                array (
+                ),
+                ),
+                1 => 
+                array (
+                'grid' => 1,
+                'index' => 0,
+                'weight' => 1,
+                'style' => 
+                array (
+                ),
+                ),
+                2 => 
+                array (
+                'grid' => 2,
+                'index' => 0,
+                'weight' => 0.5,
+                'style' => 
+                array (
+                ),
+                ),
+                3 => 
+                array (
+                'grid' => 2,
+                'index' => 1,
+                'weight' => 0.5,
+                'style' => 
+                array (
+                ),
+                ),
+            ),
 		);
 
 		return $layouts;
