952af6101aabb4b31fd060b95fd4d6c41ff63ef8
Alex Schmoe <aschmoe@gmail.com>
2021-03-25 21:39:14 -0700

    Underline text https://github.com/proudcity/wp-proudcity/issues/1828

--- modules/proud-widget/widgets/teaser-list/templates/teaser-list.php
+++ modules/proud-widget/widgets/teaser-list/templates/teaser-list.php
@@ -1,5 +1,5 @@
 <?php $teaser_list->print_list(); ?>
 
 <?php if( !empty( $instance['more_link'] ) ): ?>
-  <a href="<?php echo $instance['link_url']; ?>"><?php echo $instance['link_title']; ?></a>
+  <p><a href="<?php echo $instance['link_url']; ?>"><?php echo $instance['link_title']; ?></a></p>
 <?php endif; ?>
