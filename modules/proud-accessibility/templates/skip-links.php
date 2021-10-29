17790aac9101527122ac6aa47cffa9171f6e8494
Alex Schmoe <aschmoe@gmail.com>
2021-10-28 22:08:22 -0700

    Skip links https://github.com/proudcity/wp-proudcity/issues/1904

--- modules/proud-accessibility/templates/skip-links.php
+++ modules/proud-accessibility/templates/skip-links.php
@@ -1,5 +1,5 @@
-<ul id="skiplinks" role="navigation" aria-label="Skip links" class="sr-only">
-  <li><a href="#content-main">Skip to content</a></li>
-  <li><a href="#navbar-external">Skip to navigation</a></li>
-  <li><a href="/sitemap">Site map</a></li>
+<ul id="skiplinks" role="navigation" aria-label="Skip links">
+  <li><a href="#content-main" class="sr-only">Skip to content</a></li>
+  <li><a href="#navbar-external" class="sr-only">Skip to navigation</a></li>
+  <li><a href="/sitemap" class="sr-only">Site map</a></li>
 </ul>
