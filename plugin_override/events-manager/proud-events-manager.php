ac5c8ed99f16c89029e6a5d296f2ffeb23c7752f
Curtis McHale <curtis@curtismchale.ca>
2022-02-16 10:18:04 -0800

    working out multiday events
    We do some timestamp conversion and then divide the difference between
    start and end by 86400 (seconds in a day)

--- plugin_override/events-manager/proud-events-manager.php
+++ plugin_override/events-manager/proud-events-manager.php
@@ -34,26 +34,60 @@ function proud_events_filter( $events ){
 	$new_events_array = array();
 	foreach( $events as $event ){
 
-		$start = get_post_meta( absint( $event->post_id ), '_start_ts', true );
-		$end = get_post_meta( absint( $event->post_id ), '_end_ts', true );
+		$start_local = get_post_meta( absint( $event->post_id ), '_event_start_local', true );
+		$end_local = get_post_meta( absint( $event->post_id ), '_event_end_local', true );
+
+		$start_stamp = strtotime( $start_local );
+		$end_stamp = strtotime( $end_local );
 
 		$new_events_array[] = $event;
 
 		// if event exceets 24 hours (86400 seconds) we'll need to duplicate it
-		if ( ( $end - $start ) > 86400 ){
+		if ( ( $end_stamp - $start_stamp ) >= 86400 ){
 
-			$new_event = clone $event;
+			$how_many_days = proud_how_many_days( $end_stamp, $start_stamp );
+			echo 'how_many '. $how_many_days;
+			$counter = 0;
 
-			// adjust start date time
-			$start_date = $new_event->start_date;
-			$new_event->start_date = date( 'Y-m-d', strtotime( $start_date . '+1 day' ) );
+			while( $counter <= $how_many_days ){
+				$new_event = clone $event;
 
-			// duplicate event
-			$new_events_array[] = $new_event;
-		}
+				// adjust start date time
+				$start_date = $new_event->start_date;
+				$date_adjust = $counter + 1;
+				$new_event->start_date = date( 'Y-m-d', strtotime( $start_date . '+'.$date_adjust.' day' ) );
 
-	}
+				// duplicate event
+				$new_events_array[] = $new_event;
+
+				$counter++;
+
+			} // while
+
+		} // if $end_stamp - $start_stam
+
+	} // foreach
 
 	return $new_events_array;
-}
+
+} // proud_events_filter
 add_filter( 'proud_events_duplicator', 'proud_events_filter' );
+
+/**
+ * Returns the number of days that an event repeats so we can have events that span many days
+ *
+ * @since 2022.02.16
+ * @author Curtis
+ *
+ * @param   int         $end            required                UNIX timestamp for the end of the event
+ * @param   int         $start          required                UNIX timestamp for the start of the event
+ * @return  int         $number_of_days                         The number of days we need to repeat an event
+ */
+function proud_how_many_days( $end, $start ){
+
+	$difference = $end - $start;
+	$number_of_days = $difference / 86400;
+
+	return $number_of_days;
+
+} // proud_how_many_days
