750a8c34b2c3dd267cca16d35fd78d7d4665dceb
Curtis McHale <curtis@curtismchale.ca>
2022-05-12 10:52:06 -0700

    escaping variables

--- modules/proud-widget/widgets/teaser-list/templates/teaser-list.php
+++ modules/proud-widget/widgets/teaser-list/templates/teaser-list.php
@@ -1,5 +1,5 @@
 <?php $teaser_list->print_list(); ?>
 
 <?php if( !empty( $instance['more_link'] ) ): ?>
-  <p><a href="<?php echo $instance['link_url']; ?>"><?php echo $instance['link_title']; ?></a></p>
+  <p><a href="<?php echo esc_url( $instance['link_url'] ); ?>"><?php echo esc_attr( $instance['link_title'] ); ?></a></p>
 <?php endif; ?>
