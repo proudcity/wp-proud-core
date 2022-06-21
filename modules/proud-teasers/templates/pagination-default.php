29ca33c25052cc4608616a48259865f3c0789d7a
Curtis McHale <curtis@curtismchale.ca>
2022-06-21 11:50:45 -0700

    we have the basic pager working

--- modules/proud-teasers/templates/pagination-default.php
+++ modules/proud-teasers/templates/pagination-default.php
@@ -1,7 +1,24 @@
 <?php if ($this->query->max_num_pages > 1) { ?>
 
   <?php if ( 'document' == get_post_type() ){
-    echo 'custom pager';
+
+    $big = 9999999999;
+
+    $args = array(
+        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        'format' => '?paged=%#%',
+        'current' => max( 1, get_query_var( 'paged' ) ),
+        'total' => $this->query->max_num_pages,
+    );
+
+    echo paginate_links( $args );
+
+    /*
+    echo '<pre>';
+    print_r( $this->query );
+    echo '</pre>';
+    */
+
   } else { /* default if no custom settings */ ?>
     <nav>
       <ul class="pager prev-next-posts">
