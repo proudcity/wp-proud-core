6ae801847afe5bfc55c73963a5f25caaef39754d
Curtis McHale <curtis@curtismchale.ca>
2022-06-21 14:19:02 -0700

    working paginated navigation

--- modules/proud-teasers/templates/pagination-default.php
+++ modules/proud-teasers/templates/pagination-default.php
@@ -1,24 +1,38 @@
 <?php if ($this->query->max_num_pages > 1) { ?>
 
-  <?php if ( 'document' == get_post_type() ){
+  <?php if ( 'document' == get_post_type() ){ ?>
+    <!-- @todo use a var so we can see what type of navigation this is -->
+    <nav aria-label="Page Navigation">
+      <?php
+        $big = 9999999999;
+        $args = array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => max( 1, get_query_var( 'paged' ) ),
+            'total' => $this->query->max_num_pages,
+            'type' => 'array',
+        );
 
-    $big = 9999999999;
+        $page_array = paginate_links( $args );
 
-    $args = array(
-        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-        'format' => '?paged=%#%',
-        'current' => max( 1, get_query_var( 'paged' ) ),
-        'total' => $this->query->max_num_pages,
-    );
+        // looping through the array of links so that we can customize the HTML to our existing styles
+        echo '<ul class="pagination">';
+        foreach( $page_array as $page ){
+          echo '<li class="page-item">' . wp_kses_post( $page ) . '</li>';
+        }
+        echo '</ul>';
 
-    echo paginate_links( $args );
+        //print_r( paginate_links( $args ) );
 
-    /*
-    echo '<pre>';
-    print_r( $this->query );
-    echo '</pre>';
-    */
+        /*
+        echo '<pre>';
+        print_r( $this->query );
+        echo '</pre>';
+        */
+      ?>
+    </nav>
 
+  <?php
   } else { /* default if no custom settings */ ?>
     <nav>
       <ul class="pager prev-next-posts">
