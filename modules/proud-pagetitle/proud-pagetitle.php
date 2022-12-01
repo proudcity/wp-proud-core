b7b87583640360213131d00660dacdf58522b72f
Curtis McHale <curtis@curtismchale.ca>
2022-12-01 09:42:59 -0800

    making sure we have those $_POST vars before we check for them

--- modules/proud-pagetitle/proud-pagetitle.php
+++ modules/proud-pagetitle/proud-pagetitle.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Enqueue JS
@@ -8,13 +8,13 @@ function proud_pagetitle_admin_script(){
     wp_enqueue_script( 'proud-pagetitle', $path . '/proud-pagetitle.js', array('jquery') );
     // Set Nonce, URL
     wp_localize_script( 'proud-pagetitle', 'proud_title',
-        array( 
-            'ajax_url' => admin_url( 'admin-ajax.php' ), 
+        array(
+            'ajax_url' => admin_url( 'admin-ajax.php' ),
             '_wpnonce' => wp_create_nonce( 'proud_pagetitle_check' )
-        ) 
+        )
     );
 }
-add_action('admin_enqueue_scripts', 'proud_pagetitle_admin_script');   
+add_action('admin_enqueue_scripts', 'proud_pagetitle_admin_script');
 
 /**
  * Helper function checks if post title already exists
@@ -37,7 +37,7 @@ function proud_pagetitle_get_duplicates($title, $id) {
 function proud_pagetitle_callback() {
     // Check nonce
     if( isset( $_POST['_wpnonce'] ) && wp_verify_nonce($_POST['_wpnonce'], 'proud_pagetitle_check') ) {
-        if( proud_pagetitle_get_duplicates( $_POST['title'], $_POST['post_id'] ) ) {
+        if( isset( $_POST['title'], $_POST['post_id'] ) &&  proud_pagetitle_get_duplicates( $_POST['title'], $_POST['post_id'] ) ) {
             wp_send_json(array(
                 'duplicate_exists' => 1,
                 'duplicate_message' => __('There is already a post with this title, please choose a new one or make sure the permalink below does not already exist!', 'wp-proud-core')
