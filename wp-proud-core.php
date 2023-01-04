2b7deadb349c73de0c0dcd1a49f054baf2bf5fba
Curtis McHale <curtis@curtismchale.ca>
2023-01-04 09:46:59 -0800

    tagging new version of plugin

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2022.12.15.1315
+Version:            2023.01.04.0946
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
