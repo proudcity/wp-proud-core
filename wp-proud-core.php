d8114d0c1e229dcc72d68638d492393ecd50b8ad
Curtis McHale <curtis@curtismchale.ca>
2023-02-17 13:11:57 -0800

    updating plugin version

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2023.02.16.0728
+Version:            2023.02.17.1310
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
