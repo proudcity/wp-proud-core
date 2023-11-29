bf55e28d0e287b14cbea8892da919140217928b6
curtismchale <curtis@curtismchale.c>
2023-11-29 12:00:13 -0800

    declaring the dynamic property

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2023.09.27.0929
+Version:            2023.11.29.1157
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
