04f42bba31aa26785e7fbfda4fa514c6ee65188a
curtismchale <curtis@curtismchale.c>
2023-11-29 13:25:55 -0800

    version bump for proper php syntax

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2023.11.29.1202
+Version:            2023.11.29.1325
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
