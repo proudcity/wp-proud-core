5079254ae7ce43d5d6f92145545c6b0966db0f27
Curtis McHale <curtis@curtismchale.ca>
2023-12-12 07:03:43 -0800

    declaring dynamic properties to bring them into support with php 8.2

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2023.11.29.1325
+Version:            2023.12.12.0703
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
