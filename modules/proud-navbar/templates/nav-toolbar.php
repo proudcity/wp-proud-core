bbc0fca58c8883d1c0e702339ecbd1ec883092ce
Curtis McHale <curtis@curtismchale.ca>
2024-05-02 11:24:16 -0700

    removing the role=toolbar per pacificada accessibility

--- modules/proud-navbar/templates/nav-toolbar.php
+++ modules/proud-navbar/templates/nav-toolbar.php
@@ -1,5 +1,5 @@
 <h2 id="action-toolbar-label" class="sr-only">Action toolbar</h2><!-- wp-proud-core/modules/proud-navbar/templates/nav-toolbar.php -->
-<ul class="btn-toolbar pull-left list-unstyled clearfix" role="toolbar" aria-labelledby="action-toolbar-label">
+<ul class="btn-toolbar pull-left list-unstyled clearfix" aria-labelledby="action-toolbar-label">
     <?php do_action('proud_nav_toolbar_pre_buttons'); ?>
     <?php
     if (!get_option('proud_hide_toolbar_nav')) : ?>
@@ -29,6 +29,6 @@
             <?php endforeach; ?>
         <?php endif; ?>
 </ul>
-<ul class="btn-toolbar pull-right list-unstyled clearfix" role="toolbar">
+<ul class="btn-toolbar pull-right list-unstyled clearfix">
     <li><a title="<?php echo $search_button['title'] ?>" <?php if ($search_button['data_key']) : ?> data-proud-navbar="<?php echo $search_button['data_key'] ?>" <?php endif; ?><?php echo $search_button['data_attrs'] ?> href="<?php echo $search_button['href'] ?>" class="<?php echo $search_button['classes'] ?>"><i aria-hidden="true" class="fa <?php echo $search_button['icon'] ?>"></i> <?php echo $search_button['title'] ?></a></li>
 </ul>
