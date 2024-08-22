141e2fce94ce6b50991eb7cbbeb90b1e4f670571
curtismchale <curtis@curtismchale.c>
2024-08-22 09:47:34 -0700

    adding active aria and classes to breadcrumb links

--- modules/proud-menu/templates/breadcrumb.php
+++ modules/proud-menu/templates/breadcrumb.php
@@ -1,13 +1,15 @@
-<ol class="breadcrumb"><!-- wp-proud-core/modules/proud-menu/templates/breadcrumb.php -->
-  <?php foreach ($active_trail as $item): ?>
-    <?php if( !empty( $item['active'] ) ): ?>
-    <li class="active">
-      <?php echo $item['title'] ?>
-    </li>
-    <?php else: ?>
-    <li>
-      <a href="<?php echo $item['url'] ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
-    </li>
-    <?php endif; ?>
-  <?php endforeach; ?>
-</ol>
+<nav aria-label="Breadcrumb">
+	<ol class="breadcrumb"><!-- wp-proud-core/modules/proud-menu/templates/breadcrumb.php -->
+	<?php foreach ($active_trail as $item): ?>
+		<?php if(!empty($item['active'])): ?>
+		<li class="active">
+			<a href="" aria-current="page"><?php echo esc_attr($item['title']); ?></a>
+		</li>
+		<?php else: ?>
+			<li>
+				<a href="<?php echo esc_url($item['url']); ?>" title="<?php echo esc_attr($item['title']) ?>"><?php echo esc_attr($item['title']); ?></a>
+			</li>
+		<?php endif; ?>
+	<?php endforeach; ?>
+	</ol>
+</nav>
--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2024.08.22.0933
+Version:            2024.08.22.0942
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
