cf1af5009698c970672b8a29d0a331d10364d842
Curtis McHale <curtis@curtismchale.ca>
2025-10-14 13:39:51 -0700

    formatting for lint

--- modules/proud-widget/widgets/image-set/templates/image-cards.php
+++ modules/proud-widget/widgets/image-set/templates/image-cards.php
@@ -1,35 +1,38 @@
 <?php
   use Proud\Core;
 
-  if( !empty($imageset) ):
-  $class = ((int) $across === 3) ? 'card-columns-md-3' : 'card-columns-md-2';
-?>
+if (!empty($imageset)) :
+    $class = ((int) $across === 3) ? 'card-columns-md-3' : 'card-columns-md-2';
+    ?>
+
 <div class="card-columns card-columns-xs-2 card-columns-sm-2 <?php echo $class ?> card-columns-equalize"><!-- template-file: wp-proud-core/modules/proud-teasers/templates/image-cards.php -->
-  <?php foreach ( $imageset as $image ) : ?>
-    <?php if( !empty( $image['link_title'] ) && !empty( $image['link_url'] ) && !empty( $image['image'] ) ): ?>
-      <div class="card-wrap"><div class="card">
-        <?php if( !empty( $image['image'] ) && is_numeric( $image['image'] ) ): ?>
-        <div class="card-img-top text-center">
-          <a href="<?php print $image['link_url'] ?>">
-            <?php
-              $meta = Core\build_responsive_image_meta( $image['image'], 'card-thumb', 'card-thumb' );
-              Core\print_responsive_image( $meta, [], true );
-            ?>
-          </a>
-        </div>
+    <?php foreach ( $imageset as $image ) : ?>
+        <?php if (!empty($image['link_title']) && !empty($image['link_url']) && !empty($image['image'])) : ?>
+            <div class="card-wrap"><div class="card">
+                <?php if (!empty($image['image']) && is_numeric($image['image'])) : ?>
+                    <div class="card-img-top text-center">
+                        <a href="<?php print $image['link_url'] ?>">
+                            <?php
+                            $meta = Core\build_responsive_image_meta($image['image'], 'card-thumb', 'card-thumb');
+                            Core\print_responsive_image($meta, [], true);
+                            ?>
+                        </a>
+                    </div>
+                <?php endif; ?>
+
+                <div class="card-block">
+                    <div class="h3 margin-top-none">
+                        <a href="<?php print $image['link_url'] ?>">
+                            <?php print $image['link_title'] ?>
+                        </a>
+                    </div>
+                        <?php if (!empty($image['text'])) : ?>
+                            <p class="margin-bottom-none"><?php echo $image['text']; ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
         <?php endif; ?>
-        <div class="card-block">
-          <div class="h3 margin-top-none">
-            <a href="<?php print $image['link_url'] ?>">
-              <?php print $image['link_title'] ?>
-            </a>
-          </div>
-          <?php if( !empty( $image['text'] ) ): ?>
-          <p class="margin-bottom-none"><?php echo $image['text']; ?></p>
-          <?php endif; ?>
-        </div>
-      </div></div>
-    <?php endif; ?>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
 </div>
 <?php endif; ?>
