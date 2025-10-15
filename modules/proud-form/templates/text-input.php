7cc1e8313fd2d199850a6acce151d13aad5b04b3
Curtis McHale <curtis@curtismchale.ca>
2025-10-15 09:43:55 -0700

    formatting

--- modules/proud-form/templates/text-input.php
+++ modules/proud-form/templates/text-input.php
@@ -1,9 +1,14 @@
-<input 
-  id="<?php echo $id ?>" 
-  name="<?php echo $name ?>" type="text" 
-  value="<?php echo esc_attr( $value ); ?>"
-  <?php foreach ($args as $key=>$value): ?>
-    <?php echo $key ?>="<?php echo esc_attr( $value ); ?>"
-  <?php endforeach ?>
+<?php
+/**
+ * Renders the text field for forms
+ */
+?>
+<input
+    id="<?php echo $id ?>"
+    name="<?php echo $name ?>" type="text"
+    value="<?php echo esc_attr($value); ?>"
+    <?php foreach ($args as $key=>$value): ?>
+        <?php echo $key ?>="<?php echo esc_attr($value); ?>"
+    <?php endforeach ?>
 />
 <?php echo $after ? $after : ''; ?>
