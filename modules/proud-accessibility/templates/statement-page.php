fced7c397b53beb673f54747accf0fc008b927e2
Curtis McHale <curtis@curtismchale.ca>
2025-12-18 11:49:48 -0800

    removed dead accessibilyt link

--- modules/proud-accessibility/templates/statement-page.php
+++ modules/proud-accessibility/templates/statement-page.php
@@ -1,39 +1,38 @@
 <div class="row">
-  <div class="col-md-12 entry-content">
-    <p><?php echo $entity_name; ?> is committed to access for all.</p>
-    <p>Guided by Web Content Accessibility Guidelines (WCAG) standards, our objective is to be:</p>
-    <ol>
-      <li>Fully compliant with Section 508 and WCAG 2.0 Level AA.</li>
-      <li>Open and transparent about our process.</li>
-      <li>Attentive to community feedback surrounding usability of our website.</li>
-    </ol>
-    <p>Accessibility is an ongoing process, and here are steps we have taken:</p>
-    <ul>
-      <li>Accessible forms: "aria" attributes, descriptive labels, tab-able interfaces</li>
-      <li>Semantic markup and additional "aria" descriptors on UI components</li>
-      <li>Tab-able navigational elements</li>
-      <li>"Skiplinks" to important page elements and website sitemap</li>
-      <li>Required "alt" descriptions on images</li>
-      <li>Font size adjustment</li>
-    </ul>
-    <h2 class="h3">PDF documents</h2>
-    <p>Some of the documents that appear on this website are in PDF format. Those using assistive technology may be unable to read documents directly, unless you have the requisite plug-ins:</p>
-    <ul>
-      <li><a href="https://get.adobe.com/reader/" target="_blank">Download Acrobat Reader</a></li>
-      <li><a href="http://www.adobe.com/accessibility/" target="_blank">Access Adobe Accessibility</a></li>
-    </ul>
-    <h2 class="h3">Accessibility links</h2>
-    <ul>
-      <li><a href="https://www.w3.org/TR/WCAG/" target="_blank">WCAG Guidelines</a></li>
-      <li><a href="https://www.access-board.gov/guidelines-and-standards/communications-and-it/about-the-section-508-standards" target="_blank">Section 508</a></li>
-    </ul>
-    <h2 class="h3">Browser tools</h2>
-    <ul>
-      <li><a href="http://www.microsoft.com/enable/products/default.aspx" target="_blank">Internet Explorer Accessibility</a></li>
-      <li><a href="http://www.accessfirefox.com/" target="_blank">Firefox Accessibility</a></li>
-      <li><a href="https://www.google.com/accessibility/all-products-features.html" target="_blank">Chrome Accessibility</a></li>
-    </ul>
-    <h2 class="h3">Contact us</h2>
-    <p>Please do not hesitate to <a href="/contact">contact us</a> us with feedback on our accessibility practices.</p>
-  </div>
+	<div class="col-md-12 entry-content">
+		<p><?php echo $entity_name; ?> is committed to access for all.</p>
+		<p>Guided by Web Content Accessibility Guidelines (WCAG) standards, our objective is to be:</p>
+		<ol>
+			<li>Fully compliant with Section 508 and WCAG 2.0 Level AA.</li>
+			<li>Open and transparent about our process.</li>
+			<li>Attentive to community feedback surrounding usability of our website.</li>
+		</ol>
+		<p>Accessibility is an ongoing process, and here are steps we have taken:</p>
+		<ul>
+			<li>Accessible forms: "aria" attributes, descriptive labels, tab-able interfaces</li>
+			<li>Semantic markup and additional "aria" descriptors on UI components</li>
+			<li>Tab-able navigational elements</li>
+			<li>"Skiplinks" to important page elements and website sitemap</li>
+			<li>Required "alt" descriptions on images</li>
+			<li>Font size adjustment</li>
+		</ul>
+		<h2 class="h3">PDF documents</h2>
+		<p>Some of the documents that appear on this website are in PDF format. Those using assistive technology may be unable to read documents directly, unless you have the requisite plug-ins:</p>
+		<ul>
+			<li><a href="https://get.adobe.com/reader/" target="_blank">Download Acrobat Reader</a></li>
+		</ul>
+		<h2 class="h3">Accessibility links</h2>
+		<ul>
+			<li><a href="https://www.w3.org/TR/WCAG/" target="_blank">WCAG Guidelines</a></li>
+			<li><a href="https://www.access-board.gov/guidelines-and-standards/communications-and-it/about-the-section-508-standards" target="_blank">Section 508</a></li>
+		</ul>
+		<h2 class="h3">Browser tools</h2>
+		<ul>
+			<li><a href="http://www.microsoft.com/enable/products/default.aspx" target="_blank">Internet Explorer Accessibility</a></li>
+			<li><a href="http://www.accessfirefox.com/" target="_blank">Firefox Accessibility</a></li>
+			<li><a href="https://www.google.com/accessibility/all-products-features.html" target="_blank">Chrome Accessibility</a></li>
+		</ul>
+		<h2 class="h3">Contact us</h2>
+		<p>Please do not hesitate to <a href="/contact">contact us</a> us with feedback on our accessibility practices.</p>
+	</div>
 </div>
