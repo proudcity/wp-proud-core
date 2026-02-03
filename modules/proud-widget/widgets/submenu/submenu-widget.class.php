17599a6a715058c0f3813559a89beb38b4ce75ef
Curtis McHale <curtis@curtismchale.ca>
2026-02-03 09:28:24 -0800

    adding text card option for menu display

--- modules/proud-widget/widgets/submenu/submenu-widget.class.php
+++ modules/proud-widget/widgets/submenu/submenu-widget.class.php
@@ -2,60 +2,63 @@
 
 use Proud\Core;
 
-class Submenu extends Core\ProudWidget {
+class Submenu extends Core\ProudWidget
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct(
             'submenu', // Base ID
-            __( 'Submenu', 'wp-agency' ), // Name
-            array( 'description' => __( "Select a menu to display or, display a submenu from the Primary Menu", 'wp-agency' ), ) // Args
+            __('Submenu', 'wp-agency'), // Name
+            array('description' => __("Select a menu to display or, display a submenu from the Primary Menu", 'wp-agency'),) // Args
         );
     }
 
 
     /**
-    * Define shortcode settings.
-    *
-    * @return  void
-    */
-    function initialize() {
+     * Define shortcode settings.
+     *
+     * @return  void
+     */
+    function initialize()
+    {
         $menus = [];
         $primary = 'primary-links';
         $default = 'primary-links';
 
         // Try to grab primary
         global $proud_menu_util;
-        foreach ( $proud_menu_util::$menus as $key => $menu ) {
-            if( $key === $primary) {
-                $menus[$key] = $menu->name . __( ' (Primary Menu)', 'wp-proud-core' );
+        foreach ($proud_menu_util::$menus as $key => $menu) {
+            if ($key === $primary) {
+                $menus[$key] = $menu->name . __(' (Primary Menu)', 'wp-proud-core');
                 $default = $key;
-            }
-            else {
+            } else {
                 $menus[$key] = $menu->name;
             }
-        } 
+        }
         // No default
-        if(!$default) {
+        if (!$default) {
             reset($menus);
             $default = key($menus);
         }
 
         $this->settings += array(
-          'menu_id' => array(
-            '#title' => __( 'Menu to use', 'wp-proud-core' ),
-            '#type' => 'select',
-            '#options' => $menus,
-            '#default_value' => $default
-          ),
-          'format' => array(
-            '#title' => __( 'Format', 'wp-proud-core' ),
-            '#type' => 'radios',
-            '#options' => [
-              'sidebar' => 'Sidebar',
-              'pills' => 'Pills',
-            ],
-            '#default_value' => 'sidebar',
-          ),
+            'menu_id' => array(
+                '#title' => __('Menu to use', 'wp-proud-core'),
+                '#type' => 'select',
+                '#options' => $menus,
+                '#default_value' => $default
+            ),
+            'format' => array(
+                '#title' => __('Format', 'wp-proud-core'),
+                '#type' => 'radios',
+                '#options' => [
+                    'sidebar' => 'Sidebar',
+                    'pills' => 'Pills',
+                    'textcard' => 'Text Card',
+                ],
+                '#default_value' => 'sidebar',
+            ),
         );
     }
 
@@ -67,8 +70,9 @@ class Submenu extends Core\ProudWidget {
      * @param array $args     Widget arguments.
      * @param array $instance Saved values from database.
      */
-    public function hasContent($args, &$instance) {
-        $instance['menu_class'] = new Core\ProudMenu( $instance['menu_id'], $instance['format'] );
+    public function hasContent($args, &$instance)
+    {
+        $instance['menu_class'] = new Core\ProudMenu($instance['menu_id'], $instance['format']);
         return true;
     }
 
@@ -78,14 +82,17 @@ class Submenu extends Core\ProudWidget {
      * @param array $args
      * @param array $instance
      */
-    public function printWidget( $args, $instance ) {
+    public function printWidget($args, $instance)
+    {
         extract($instance);
         $menu_class->print_menu();
     }
 }
 
 // register Foo_Widget widget
-function register_submenu_widget() {
-    register_widget( 'Submenu' );
+function register_submenu_widget()
+{
+    register_widget('Submenu');
 }
-add_action( 'widgets_init', 'register_submenu_widget' );
+add_action('widgets_init', 'register_submenu_widget');
+
