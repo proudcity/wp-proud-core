74c226136e9e459b62c79fa2197b33c55dd00f92
Curtis McHale <curtis@curtismchale.ca>
2026-02-03 09:28:16 -0800

    linter

--- modules/proud-widget/widgets/contact-block/templates/contact-block.php
+++ modules/proud-widget/widgets/contact-block/templates/contact-block.php
@@ -1,11 +1,11 @@
-<?php echo Proud\Core\sanitize_input_text_output($contact_html); ?>
+<?php echo Proud\Core\sanitize_input_text_output($contact_html); ?><!-- wp-proud-core/modules/proud-widget/widgets/contact-block/templates/contact-block.php -->
 <label class="sr-only">Contact Links</label>
 <ul class="list-inline margin-bottom-none">
-  <li><a href="/contact">Directory</a></li>
-  <li><a href="/feedback">Feedback</a></li>
+    <li><a href="/contact">Directory</a></li>
+    <li><a href="/feedback">Feedback</a></li>
 </ul>
 <label class="sr-only">Access Links</label>
 <ul class="list-inline">
-  <li><a href="/accessibility-statement">Accessibility</a></li>
-  <li><a href="/sitemap">Sitemap</a></li>
+    <li><a href="/accessibility-statement">Accessibility</a></li>
+    <li><a href="/sitemap">Sitemap</a></li>
 </ul>
