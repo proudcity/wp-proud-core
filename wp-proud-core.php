c90c87877e419c4363c852e215458dce5c8c5ed3
Curtis McHale <curtis@curtismchale.ca>
2026-02-25 11:39:37 -0800

    bumping version

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2026.02.17.1133
+Version:            2026.02.25.1139
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
