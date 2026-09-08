0acc605f5a821f7e2dead08c1c743cf143bf4eab
Curtis McHale <curtis@curtismchale.ca>
2026-09-08 11:53:20 -0700

    version 2026.09.08.1151

--- wp-proud-core.php
+++ wp-proud-core.php
@@ -3,7 +3,7 @@
 Plugin Name:        Proud Core
 Plugin URI:         http://getproudcity.com
 Description:        ProudCity distribution
-Version:            2026.09.04.1018
+Version:            2026.09.08.1151
 Author:             ProudCity
 Author URI:         http://getproudcity.com
 
